f4ec7d5ef49a844b5420d4893e382acd63e47e89
Karma <92379424+Karma192@users.noreply.github.com>
2023-01-09 18:10:52 +0100

    Revert "Def du Player"
    This reverts commit bcda0b54e8a0af2a0dfb6d439a428746f54f0f44.

--- GrandProjet-GTECH2/src/Player.hpp
+++ GrandProjet-GTECH2/src/Player.hpp
@@ -3,7 +3,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Config.hpp>
 #include <vector>
-#include <string>
 
 #define PLAYER_TEXTURE "ressources/sprites/player/idle.png"
 #define PLAYER_IDLE {sf::IntRect(0, 0, 54, 52), sf::IntRect(69, 0, 54, 52),sf::IntRect(130, 0, 54, 52), sf::IntRect(195, 0, 54, 52) }
@@ -15,40 +14,14 @@ public:
 	virtual ~Player();
 
 	void PlayerLoop();
+	void PlayerTest(sf::RenderWindow* window);
 	void PlayerRender(sf::RenderWindow* window);
 	int frame = 0;
 
 	sf::Texture playerTexture;
 	sf::Sprite playerSprite;
 
-	void setPv(int pv) { m_pv = pv; }
-	int getPv() const { return m_pv; }
-	void setAttack(int attack) { m_attack = attack; }
-	int getAttack() const { return m_attack; }
-
-	void setSpeed(int speed) { m_speed = speed; }
-	int getSpeed() const { return m_speed; }
-	void setAttackSpeed(int attackspeed) { m_attackspeed = attackspeed; }
-	int getAttackSpeed() const { return m_attackspeed; }
-	float base_attack;
-
-	//std::vector<const char> spells;
-	std::vector<const char> ressources;
-
-	float attack_speed;
-	float speed;
-	float speed_projectile;
-
-	int gold;
-	int key;
-	float luck;
-
-
 private:
 	sf::Clock clock;
 
-	int m_pv;
-	int m_attack;
-	int m_speed;
-	int m_attackspeed;
 };
