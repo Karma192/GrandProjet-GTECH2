114da831f02e3fc9b944341231a53335b9ddd118
Pandouiille <92433601+Pandouiille@users.noreply.github.com>
2023-01-27 11:57:12 +0100

    commit

--- GrandProjet-GTECH2/src/Player.hpp
+++ GrandProjet-GTECH2/src/Player.hpp
@@ -16,18 +16,17 @@ public:
 	void PlayerLoop();
 	void PlayerTest(sf::RenderWindow* window);
 	void PlayerRender(sf::RenderWindow* window);
-	int frame = 0;
 
-	void setPv(int pv) { m_pv = pv; }
-	int getPv() const { return m_pv; }
+	//Player HUD 
 
-	//void takeDamage(int damage) { m_pv -= damage; if (m_pv < 0) m_pv = 0; }
+	void playerEndurance();
+	void playerRegenEndurance();
+	void playerUI();
 
-	void ControllerInput();
-
-	void ControllerMove();
-
-	void MovePlayer();
+	//rectangle pour endurance + vie
+	sf::RectangleShape enduranceBar;
+	sf::RectangleShape enduranceBarBack;
+	sf::RectangleShape lifeBar;
 
 	//cercle pour ulti + spell
 	sf::CircleShape playerUltiUI;
@@ -35,45 +34,45 @@ public:
 	sf::CircleShape playerSecondSpell;
 	sf::CircleShape playerThirdSpell;
 
-	//rectangle pour endurance + vie
-	sf::RectangleShape enduranceBar;
-	sf::RectangleShape enduranceBarBack;
-	sf::RectangleShape lifeBar;
+
+
+	int frame = 0;
+
+	void setPv(int pv) { m_pv = pv; }
+	int getPv() const { return m_pv; }
 	void setAttack(int attack) { m_attack = attack; }
 	int getAttack() const { return m_attack; }
 
-	void setSpeed(int speed) { m_speed = speed; }
-	int getSpeed() const { return m_speed; }
+	///Exemples d'applications des fonctions des PV/Attaque au dessus (Pandouille) ///
 
-	//test
-	sf::RectangleShape cube;
-	sf::Vector2f moveSpeed;
-	void setAttackSpeed(int attackspeed) { m_attackspeed = attackspeed; }
-	int getAttackSpeed() const { return m_attackspeed; }
+	/*player.setPv(100);
+	player.setAttack(20);
 
-	void setGold(int gold) { m_gold = gold; }
-	int getGold() const { return m_gold; }
+	int pv = player.getPv();
+	int attack = player.getAttack();
+	void takeDamage(int damage)
+	{
+		m_pv -= damage;
+		if (m_pv < 0) m_pv = 0;
+	}
 
-	void setKeys(int keys) { m_keys = keys; }
-	int getKeys() const { return m_keys; }
+	*/
 
-	void setLuck(int luck) { m_luck = luck; }
-	int getLuck() const { return m_luck; }
-	
-	
-	
-	
 	sf::Texture playerTexture;
 	sf::Sprite playerSprite;
 
-private:
-	sf::Clock clock;
+protected:
+	float endurancePlayer = 100;
+	float cd_Endurance = endurance.getElapsedTime().asSeconds();
 
+
+private:
+
+	sf::Clock clock;
 	int m_pv;
 	int m_attack;
-	int m_speed;
-	int m_attackspeed;
-	int m_gold;
-	int m_keys;
-	int m_luck;
+
+	sf::Clock endurance;
+
+
 };
