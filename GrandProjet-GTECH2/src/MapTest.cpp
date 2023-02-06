4e4a08526263e99bade8e96aade8be242cb8a067
Karma <agrandpaul@gaming.tech>
2023-02-06 17:39:02 +0100

    first commit

---
+++ GrandProjet-GTECH2/src/MapTest.cpp
@@ -0,0 +1,22 @@
+#include "MapTest.hpp"
+
+Map::Map()
+{
+    village.load("village.tmx");
+    layerZero = new MapLayer(village, 0);
+    layerOne = new MapLayer(village, 1);
+    layerTwo = new MapLayer(village, 2);
+}
+
+Map::~Map()
+{
+
+}
+
+void Map::Render()
+{
+    gameData = GetGameData();
+    gameData.window->draw(*layerZero);
+    gameData.window->draw(*layerOne);
+    gameData.window->draw(*layerTwo);
+}
---
+++ GrandProjet-GTECH2/src/MapTest.hpp
@@ -0,0 +1,18 @@
+#include <tmxlite/Map.hpp>
+#include "GameObject.hpp"
+#include "../../SFMLExample/src/SFMLOrthogonalLayer.hpp"
+
+class Map : public GameObject 
+{
+public:
+	Map();
+	virtual ~Map();
+
+	virtual void Render()override;
+
+private:
+	tmx::Map village;
+	MapLayer* layerZero;
+	MapLayer* layerOne;
+	MapLayer* layerTwo;
+};
