6db62e8ad18ed3e974e6ec96ae52bce17a6119c6
Karma <92379424+Karma192@users.noreply.github.com>
2023-03-30 13:44:46 +0200

    lil miss

--- GrandProjet-GTECH2/src/CSPurpose/PhysicsManager.cpp
+++ GrandProjet-GTECH2/src/CSPurpose/PhysicsManager.cpp
@@ -24,7 +24,7 @@ void PhysicsManager::Update()
 {
 #ifdef _DEBUG
 	//std::cout << "Body count : " << _bodies.size() << std::endl;
-	std::cout << "x : " << _bodies[1]->Hitbox()->left << "y : " << _bodies[1]->Hitbox()->top << std::endl;
+	std::cout << "x : " << _bodies[1]->Hitbox()->left << "\ty : " << _bodies[1]->Hitbox()->top << std::endl;
 #endif // _DEBUG
 
 	for (int i = 0; i < _bodies.size(); i++)
@@ -32,8 +32,7 @@ void PhysicsManager::Update()
 		for (int j = i + 1; j < _bodies.size() - 1; j++)
 		{
 			if (_bodies[i]->Hitbox() != nullptr && _bodies[j]->Hitbox() != nullptr
-				&& _bodies[i]->GetTag() != _bodies[j]->GetTag()
-				&& _bodies[i] != _bodies[j]
+				&& !_bodies[i]->CompareTag(_bodies[j]->GetTag())
 				&& _bodies[i]->Hitbox()->intersects(*_bodies[j]->Hitbox()))
 			{
 				_bodies[i]->OnCollisionEnter(_bodies[j]);
